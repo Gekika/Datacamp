4205f0362c5ad1c7fa8d1e0afda56fd4f25f2619
Gekika <gkkaranja2@gmail.com>
2026-09-24 20:29:27 +0300

    Introduction to python by Datacamp

--- Python-DataScience/1 - Intro-to-python/Python-Lists.py
+++ Python-DataScience/1 - Intro-to-python/Python-Lists.py
@@ -7,8 +7,15 @@ str - string, text
 bool - True, False
 """
 # we can create python lists using square brackets
-#  a list is a way to give name collection of values
+#  a list is a way to give name a collection of values
 # we can have any type in lists
 #  we can have lists inside Lists 
 [45,56,67]
- 
+
+# we can access items in a List using indexes
+# python uses zero indexing
+# we can access from the end using - indexing
+
+# we also get  -  allows selecting multiple items from a list 
+# slicing syntax [start(inclusive):end(exclusive)]
+
