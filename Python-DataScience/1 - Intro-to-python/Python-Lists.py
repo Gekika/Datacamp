8881274d2a4a3c4d212f1733f1fc779e5627a268
Gekika <gkkaranja2@gmail.com>
2026-09-24 07:39:21 +0300

    Introdcution to python y Datacamp

---
+++ Python-DataScience/1 - Intro-to-python/Python-Lists.py
@@ -0,0 +1,14 @@
+# Python Lists
+"""
+Python Data Types
+float - real numbers
+int - integer numbers
+str - string, text
+bool - True, False
+"""
+# we can create python lists using square brackets
+#  a list is a way to give name collection of values
+# we can have any type in lists
+#  we can have lists inside Lists 
+[45,56,67]
+ 
