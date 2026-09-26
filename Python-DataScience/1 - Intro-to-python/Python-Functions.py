082ed69111fec94f7bdb11c5134cffcd86cd87ca
Gekika <gkkaranja2@gmail.com>
2026-09-26 19:41:04 +0300

    deep into functions and methods

---
+++ Python-DataScience/1 - Intro-to-python/Python-Functions.py
@@ -0,0 +1,28 @@
+# this is a piece of reusable code aimed at solving a particular task
+# Python has a number of pre written fucntions to use
+
+# eg round() - takes 2 argunments to round off numbers
+print(round(2.655357,4 ))
+
+# incase you are doing any standar tasks just know probably there is a function cretaed for it - internet is my friend
+
+# if you know the name of a function and dont know how to use it use the help() function 
+# in ipython we can use ? inplace of the help keyword
+
+# we can see optional argument when the values are passed try to look at pow doc and learn
+
+# METHODS
+# python objects also comes with methods
+# methods are basically functions that belong to a particular object
+
+# eg we haev list methods which we can use in the list sample are count, index etc 
+# everything in python is an object and different objects have different methods associated with
+
+# just take note that all types haev different methods learn more ...
+
+# PACKAGES
+# We can think of it as a directory of python scripts, each scripts being a so called module, a module specifying fucntions methods and types 
+# not all packages are built in ..to use some of them you have to install them and in teh code tell the program when and how to use them
+# pip - package maintenance system for python pip 
+# after installing the package you import it to use it 
+#   when in need of fucntions we can import the whole package eg import math or we can do selective import - from math import pi
